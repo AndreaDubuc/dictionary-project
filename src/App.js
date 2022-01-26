1fa363f8d12a18c40876e052d545c07c3fdab879
Andrea Dubuc <dubuc.andrea@gmail.com>
2022-01-25 22:04:25 -0500

    Created search function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import flower from "./flower.png";
 import './App.css';
+import Dictionary from "./Dictionary";
+import React from "react";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
     <img src={flower} className="flower"
     alt="purple flower"/>
       </header>
+      <main>
+       <Dictionary /> 
+      </main>
+      <footer className="App-footer">
+        <small>
+           Coded by Andrea Dubuc
+           </small>
+      </footer>
+    </div>
     </div>
   );
 }
