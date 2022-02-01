6ce8ba660eca087c761b977820538780083cab6d
Andrea Dubuc <dubuc.andrea@gmail.com>
2022-01-31 19:38:09 -0500

    simplifying anchor tag in phonetic.js

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,9 +4,7 @@ export default function Phonetic(props) {
   console.log(props.phonetic);
   return (
     <div className="Phonetic d-flex flex-row">
-      <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        Listen
-      </a>
+      <a href={props.phonetic.audio}>Listen</a>
       <br />
       <span className="text my-2"> /{props.phonetic.text}/ </span>
     </div>
