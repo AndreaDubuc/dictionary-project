241cb081ea3d8bb000d8ada62f8d970435acb6ab
Andrea Dubuc <dubuc.andrea@gmail.com>
2022-02-02 21:18:57 -0500

    Changes to footer

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Dictionary from "./Dictionary";
 import React from 'react';
 
 
-
 export default function App() {
   return (
     <div className="App">
@@ -16,7 +15,28 @@ export default function App() {
           <Dictionary defaultKeyword="euphoria" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Andrea Dubuc</small>
+          <small className="small">
+            <a
+              className="links"
+              href="https://github.com/AndreaDubuc/dictionary-project"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {' '}
+              Open source code{' '}
+            </a>{' '}
+            by {''}
+            <a
+              className="links"
+              href="https://www.instagram.com/_andreacodes/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {' '}
+              Andrea Dubuc{' '}
+            </a>{' '}
+            2022
+          </small>
         </footer>
       </div>
     </div>
